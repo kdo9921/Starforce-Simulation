822ff94681532c178f23e06b0579008dee007c6e
키위 <kdo9921@apteryx.moe>
2019-12-28 15:38:28 +0900

    스타포스 시뮬레이션 2.0
    할인 추가, 이벤트 추가, 로직 개선

--- starforce.js
+++ starforce.js
@@ -1,4 +1,4 @@
-var maxStarArr = [8,10,15,20,25,25,25,25];
+const maxStarArr = [8,10,15,20,25,25,25,25];
 var chance = 0;
 var destroyed = 0;
 var noDestCheck = 0;
@@ -9,7 +9,14 @@ var maxStar;
 var levelIndex;
 var tryCount = 0;
 var starforce = 0; //현재 강화수치
-var costDB = [
+var pcCafeDC = 0;
+var totalDC = 1;
+var mvp = 0;
+var dc30p = false;
+var onePlusOne = false;
+var ftft100p = false;
+var freeNoDest = false;
+const costDB = [
     [41000, 81000, 121000, 161000, 201000, 241000, 281000, 321000, 0],//100
     [54200, 107500, 160700, 214000, 267200, 320400, 373700, 426900, 480200, 533400, 0],//110
     [70100, 139200, 208400, 277500, 346600, 415700, 484800, 554000, 623100, 692200, 2801600, 3543200, 4397700, 5371700, 6471400, 0],//120
@@ -20,9 +27,9 @@ var costDB = [
     [321000, 641000, 1281000, 1601000, 1921000, 2241000, 2561000, 2881000, 3201000, 12966500, 16400100, 20356300, 24865300, 29956500, 71316500, 83999600, 98016700, 113422300, 130270000, 148612400, 168501500, 189988600, 213124000, 237957700, 0] //200
 ];
 
-var itemImgDB = ["100", "110", "120", "130", "140", "150", "160", "200"]
+const itemImgDB = ["100", "110", "120", "130", "140", "150", "160", "200"]
 
-var percentDB = [   //[성공, 실패, 하락, 파괴]
+const percentDB = [   //[성공, 실패, 하락, 파괴]
     [950, 50, 0, 0], //0->1
     [900, 100, 0, 0],
     [850, 150, 0, 0],
@@ -62,13 +69,18 @@ function levelApply(){
     maxStar = maxStarArr[levelIndex];
     return level;
 }
+
 function setInfo(){
-    if ((starforce < 12) || (starforce > 16)) {
-        document.getElementById("noDest").disabled = true;
-    } else {
-        document.getElementById("noDest").disabled = false;
-    }
     var noDest = document.getElementById("noDest");
+    if ((starforce < 12) || (starforce > 16)) {
+        noDest.disabled = true;
+    } else {
+        noDest.disabled = false;
+    }
+    if (freeNoDest && starforce < 15) {
+        noDest.disabled = true;
+        noDest.checked = false;
+    }
     if (noDest.checked) {
         noDestCheck = 1;
     } else {
@@ -79,8 +91,7 @@ function setInfo(){
         document.getElementById("noDest").value = "false";
     }
     starCatch = 10 * document.getElementById("catchPercent").value;
-    totalUse += cost;
-    cost = costDB[levelIndex][starforce];
+    cost = costDB[levelIndex][starforce] * totalDC;
     var notice = document.getElementById("notice");
     var itemImg = document.getElementById("itemImg");
     var currentStar = document.getElementById("star");
@@ -91,7 +102,7 @@ function setInfo(){
     var tryCountNotice = document.getElementById("tryCount");
     tryCountNotice.innerHTML = addComma(tryCount);
     if (11 < starforce && starforce < 17 && noDestCheck) {
-        cost = cost * 2;
+        cost = cost + costDB[levelIndex][starforce];
     }
     var starHTML = "";
     for (var i = 0; i < maxStar; i++) {
@@ -108,10 +119,17 @@ function setInfo(){
         }
     }
     currentStar.innerHTML = starHTML;
-    price.innerHTML = addComma(cost);
+    if (cost < costDB[levelIndex][starforce]) {
+        price.innerHTML = "<span style='color:#999999;'><s>" + addComma(costDB[levelIndex][starforce]) + "</s></span>&nbsp;&nbsp;" + addComma(cost);
+    } else {
+        price.innerHTML = addComma(cost);
+    }
     totalUsedMeso.innerHTML = addComma(totalUse);
     itemImg.style.backgroundImage = "url('./img/" + itemImgDB[levelIndex] + ".png')"
     infoTextValue = starforce + "성 > " + (starforce + 1) + "성<br>";
+    if (onePlusOne && starforce < 11) {
+        infoTextValue = starforce + "성 > " + (starforce + 2) + "성<br>";
+    }
     if (chance == 2) {
         notice.innerHTML = "찬스타임!"
         infoTextValue += "성공확률 100%"
@@ -128,12 +146,15 @@ function setInfo(){
         }
         infoTextValue += "성공확률 : " + (percentDB[starforce][0] * 0.1).toFixed(1) + "%";
         if (starforce < 11 || starforce == 15 || starforce == 20){
-            infoTextValue += "<br>실패(유지)확률 : " + (percentDB[starforce][1] * 0.1).toFixed(1) + "%";
+            infoTextValue += "<br>실패(유지)확률 : " +((freeNoDest?percentDB[starforce][1]+percentDB[starforce][3]:percentDB[starforce][1]) * 0.1).toFixed(1) + "%";
         } else {
-            infoTextValue += "<br>실패(하락)확률 : " + (percentDB[starforce][2] * 0.1).toFixed(1) + "%";
+            infoTextValue += "<br>실패(하락)확률 : " + ((freeNoDest?percentDB[starforce][2]+percentDB[starforce][3]:percentDB[starforce][2]) * 0.1).toFixed(1) + "%";
         }
-        if (starforce > 11) {
+        if (starforce > 11 && !freeNoDest) {
             infoTextValue += "<br>파괴확률 : " + (percentDB[starforce][3] * 0.1).toFixed(1) + "%";
+        }
+        if (ftft100p && starforce < 16 && starforce % 5 == 0) {
+            infoTextValue = starforce + "성 > " + (starforce + 1) + "성<br>성공확률 100%";
         }
     }
     dest.innerHTML = addComma(destroyed);
@@ -144,6 +165,7 @@ function setInfo(){
 }
 function force(){
     tryCount += 1;
+    totalUse += cost;
     if (starforce == 0) {
         levelApply();
     }
@@ -155,17 +177,23 @@ function force(){
         return 0;
     }
     var luck = Math.floor(Math.random() * 1000) + 1;
-
     if ((luck < starCatch + percentDB[starforce][0]) || chance == 2) {
         starforce += 1; //강화성공
+        if (onePlusOne && starforce < 12) {
+            starforce += 1;
+        }
         chance = 0;
-    } else if ((luck - percentDB[starforce][0]) < percentDB[starforce][1]) {
+    }   else if (ftft100p && starforce < 16 && starforce % 5 == 0) {
+        starforce += 1; //강화성공
+        chance = 0;
+    } 
+    else if ((luck - percentDB[starforce][0]) < percentDB[starforce][1]) {
         starforce += 0; //강화실패, 등급유지
     } else if ((luck - percentDB[starforce][0] - percentDB[starforce][1]) < percentDB[starforce][2]) {
         starforce += -1; //강화실패, 등급하락
         chance += 1;
     } else {
-        if (noDestCheck) {
+        if (noDestCheck || freeNoDest) {
             if (percentDB[starforce][1] > 0) {
                 starforce += 0; //강화실패, 등급유지
             } else {
@@ -192,7 +220,33 @@ function applyBtn(){
     levelApply();
     setInfo();
 }
-function optionBtn(){
-    totalUse -= cost;
+function getTotalDC() {
+    var mvpSelect = document.getElementById("mvp");
+    mvp = Number(mvpSelect.options[mvpSelect.selectedIndex].value);
+    totalDC = 1 - ((mvp + pcCafeDC) / 100);
+    if (dc30p) {
+        totalDC = totalDC * 0.7;
+    }
     setInfo();
+}
+
+function applyPCcafeDC() {
+    var isPCcafe = document.getElementById('PCcafe');
+    if (isPCcafe.checked) {
+        pcCafeDC = 10;
+    } else {
+        pcCafeDC = 0;
+    }
+    getTotalDC();
+}
+
+function reset() {
+    chance = 0;
+    destroyed = 0;
+    noDestCheck = 0;
+    cost = 0;
+    totalUse = 0;
+    tryCount = 0;
+    starforce = 0;
+    getTotalDC();
 }
